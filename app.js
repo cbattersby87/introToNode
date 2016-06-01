296ae48a5d1d7f4d46529e2adc45cada27b62f00
Charlotte Battersby <Charlotte.Battersby@hunterindustries.com>
2016-06-01 08:52:08 -0700

    Testing request and response, also cleaned up Hello world text to remove /n

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
-    response.end('Hello World/n');
+    response.end('Hello World');
     
 }).listen(8081);
 
